efa0f8cd8a790003eeb0b4f67a953863f989ce29
GustavoMaier <43315074+GustavoMaier@users.noreply.github.com>
2018-09-16 12:41:58 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$banco = "ecommerce";
+$host = "us-cdbr-iron-east-01.cleardb.net";
+$user = "b27e60e2fd0f83";
+$pass = "cd549ecd";
+$banco = "ad_d5175bc9b657c47";
 $conexao = mysql_connect ( $host, $user, $pass ) or die ( mysql_error () );
 mysql_select_db ( $banco ) or die ( mysql_error () );
 ?>
